0ec5d53e3a18f473ecb52b0a50af46cdcc52f13a
nayoung511 <nayoungbae511@gmail.com>
2020-09-18 06:36:42 +0900

    practice ~030

---
+++ day2/016.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char ch1 = 'c';
+    char c2 = 200;
+
+    unsigned char ch3 = 'c';
+    unsigned char ch4 = 200;
+
+    printf("char ch1 = %c, %d\n", ch1, ch1);
+    printf("char ch1 = %c, %d\n", ch2, ch2);
+    printf("char ch1 = %c, %d\n", ch3, ch3);
+    printf("char ch1 = %c, %d\n", ch4, ch4);
+}
